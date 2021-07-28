4d8274c0d56182cbec2a936e6e5aa6468fdb98d3
maxfnine <maxfnine@gmail.com>
2021-07-28 06:31:56 +0300

    Working but not complete version

--- src/main/java/com/example/scraper/scraper/WebScraper.java
+++ src/main/java/com/example/scraper/scraper/WebScraper.java
@@ -9,6 +9,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import javax.annotation.PreDestroy;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -20,6 +25,7 @@ public class WebScraper {
 	private int maximumUrls;
 	private int depth;
 	private boolean isUnique;
+	private ExecutorService executorService;
 	
 	
 
@@ -60,54 +66,68 @@ public class WebScraper {
 		this.maximumUrls = maximumUrls;
 		this.depth = depth;
 		this.isUnique = isUnique;
+		this.executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 	}
 
 	public WebScraper() {
-		
+	}
+	
+	@PreDestroy
+	private void disposeExecutor() {
+		System.out.println("SHUTDOWN!!!");
+		executorService.shutdown();
 	}
 
 	public void scrapeLinks() {
 		try {
 
 			Map<Integer, List<String>> levelLinks = new HashMap<Integer, List<String>>();
-			List<Runnable> tasks = new ArrayList<Runnable>();
+			
 			
 			Set<String> storedLinks = new HashSet<String>();
 			for (int i = 0; i < depth; i++) {
 				if (i == 0) {
-					Document doc = Jsoup.connect(initialUrl).get();
-					doc.getElementsByTag("a");
-					List<Element> links = doc.getElementsByTag("a");
 					List<String> currentLevelLinks = new ArrayList<String>();
 					levelLinks.put(i, currentLevelLinks);
-					tasks.add(new RunnableTask(i, initialUrl, doc.toString()));
-					extractcurrentLevelLinks(storedLinks, currentLevelLinks, links);
+					extractCurrentLevelLinks(initialUrl, storedLinks, i, currentLevelLinks);
 
-				} else {
+				} else{
 					List<String> prevLevelLinks = levelLinks.get(i - 1);
 					List<String> currentLevelLinks = new ArrayList<String>();
 					levelLinks.put(i, currentLevelLinks);
 					for (String prevLevelLink : prevLevelLinks) {
-						Document doc = Jsoup.connect(prevLevelLink).get();
-						doc.getElementsByTag("a");
-						List<Element> links = doc.getElementsByTag("a");
-						tasks.add(new RunnableTask(i, prevLevelLink, doc.toString()));
-						extractcurrentLevelLinks(storedLinks, currentLevelLinks, links);
+						extractCurrentLevelLinks(prevLevelLink, storedLinks, i, currentLevelLinks);
 
 					}
 				}
 				System.out.println(levelLinks);
-				tasks.forEach(task -> new Thread(task).start());
 			}
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
+	private void extractCurrentLevelLinks(String urlToScrape, Set<String> storedLinks, int i,List<String> currentLevelLinks) throws IOException 
+	{
+		List<Runnable> tasks = new ArrayList<Runnable>();
+		Document doc = Jsoup.connect(urlToScrape).get();
+		List<Element> links = doc.getElementsByTag("a");
+		tasks.add(new RunnableTask(i, urlToScrape, doc.toString()));
+		extractcurrentLevelLinks(storedLinks, currentLevelLinks, links);
+		storeUrlContents(tasks);
+	}
+
+	private void storeUrlContents(List<Runnable> tasks) {
+		for(Runnable task:tasks) {
+			executorService.execute(task);
+		}
+		
+	}
+
 	private void extractcurrentLevelLinks(Set<String> storedLinks, List<String> currentLevelLinks, List<Element> links) {
+		int linksInserted=0;
 		for (Element link : links) {
 			String hrefLink = link.attr("href").trim();
 			if (!hrefLink.isEmpty() && (hrefLink.startsWith("http") || hrefLink.startsWith("https"))) {
@@ -115,12 +135,14 @@ public class WebScraper {
 					if (!storedLinks.contains(hrefLink)) {
 						storedLinks.add(hrefLink);
 						currentLevelLinks.add(hrefLink);
+						linksInserted++;
 					}
 				} else {
 					currentLevelLinks.add(hrefLink);
+					linksInserted++;
 				}
 
-				if (currentLevelLinks.size() == maximumUrls) {
+				if (linksInserted == maximumUrls) {
 					break;
 				}
 
@@ -151,7 +173,6 @@ public class WebScraper {
 				writer.write(urlContent);
 				writer.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}	    
 			
